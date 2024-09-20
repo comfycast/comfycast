93be2c143c6e362caf01227834a11ddceb6c0abd
Federico Maria Morrone <contact@morrone.dev>
2024-09-20 15:48:44 +0200

    Add openapi generation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,43 @@
 use anyhow::Result;
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
 use tokio::net::TcpListener;
 use tracing::info;
+use utoipa::OpenApi;
+use utoipa_scalar::{Scalar, Servable as _};
 
 const ADDR: &str = "0.0.0.0:9191";
+
+#[derive(OpenApi)]
+#[openapi(paths(pong))]
+struct ApiDoc;
+
+async fn openapi() -> Json<utoipa::openapi::OpenApi> {
+    Json(ApiDoc::openapi())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/api/v1/openapi.json", get(openapi))
+        .merge(Scalar::with_url("/api", ApiDoc::openapi()))
+        .route("/api/v1/ping", get(pong));
 
     info!("Started server at \"{ADDR}\"");
     let listener = TcpListener::bind(ADDR).await?;
     axum::serve(listener, app).await?;
 
     Ok(())
+}
+
+#[utoipa::path(
+    get,
+    path = "/api/v1/ping",
+    responses(
+        (status = 200, description = "Returns pong")
+    )
+)]
+async fn pong() -> &'static str {
+    "pong"
 }
