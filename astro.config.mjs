cdb00594916ef1d47b7bc092a92fcda644a1656c
Matilde Morrone <contact@morrone.dev>
2024-11-01 15:12:22 +0100

    Add vidstack player

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+import { vite as vidstack } from "vidstack/plugins";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  vite: {
+    plugins: [vidstack()],
+  },
 });
