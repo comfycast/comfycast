299d4c02551e8be52e70c669a9f2f87028997a47
Matilde Morrone <contact@morrone.dev>
2024-11-03 19:08:59 +0100

    Use hls.js to reproduce videos

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,7 +5,9 @@ import {
   Suspense,
   type Component,
   Show,
+  createEffect,
 } from "solid-js";
+import HLS from "hls.js";
 
 interface StreamResponse {
   url: string;
@@ -16,8 +18,18 @@ const fetchStream = async (id: string): Promise<StreamResponse> => {
   return response.json();
 };
 
-const Player: Component<{ id: string }> = (props) => {
+const VideoPlayer: Component<{ id: string }> = (props) => {
   const [stream] = createResource(props.id, fetchStream);
+
+  let video!: HTMLVideoElement;
+
+  createEffect(() => {
+    if (stream()) {
+      const hls = new HLS();
+      hls.loadSource(stream()!.url);
+      hls.attachMedia(video);
+    }
+  });
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -28,10 +40,12 @@ const Player: Component<{ id: string }> = (props) => {
         <Match when={stream.error}>
           <span>Error: {stream.error}</span>
         </Match>
-        <Match when={stream()}>{stream()!.url}</Match>
+        <Match when={stream()}>
+          <video ref={video} class="h-200" controls></video>
+        </Match>
       </Switch>
     </Suspense>
   );
 };
 
-export default Player;
+export default VideoPlayer;
