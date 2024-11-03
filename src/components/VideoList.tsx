b543aa32835a0c7e93b1a0861e2ffe1d3f3aa401
Matilde Morrone <contact@morrone.dev>
2024-11-03 20:01:04 +0100

    Update video resource

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -2,6 +2,7 @@ import ky from "ky";
 import { createResource, type Component, For } from "solid-js";
 
 interface Video {
+  name: string;
   id: string;
 }
 
@@ -12,9 +13,15 @@ const VideoList: Component = () => {
   const [videos] = createResource(fetchVideos);
 
   return (
-    <For each={videos()} fallback={<div>Loading...</div>}>
-      {(video) => <div>{video.id}</div>}
-    </For>
+    <div class="flex flex-col p-5">
+      <For each={videos()} fallback={<div>Loading...</div>}>
+        {(video) => (
+          <button class="border-1 m-1 h-20 border border-black shadow-sm">
+            {video.name}
+          </button>
+        )}
+      </For>
+    </div>
   );
 };
 
