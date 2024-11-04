16a1aa9a4e99f07890b818ba5f99c166fc3769e3
Federico Maria Morrone <contact@morrone.dev>
2024-11-04 16:00:45 +0100

    Use image passthrough service

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import deno from "@deno/astro-adapter";
 
@@ -7,4 +7,7 @@ export default defineConfig({
   output: "server",
   adapter: deno(),
   integrations: [tailwind()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
