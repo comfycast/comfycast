ee3aa137614f12c812836debf9360a87e9adfb79
Matilde Morrone <contact@morrone.dev>
2024-11-04 15:53:48 +0100

    Add deno as server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: deno(),
   integrations: [tailwind()],
 });
